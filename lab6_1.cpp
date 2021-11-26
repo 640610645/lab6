49681adc7c6b8618bef0d641bb173918a5c3eee5
640610645 <nontapan_chanadee@cmu.ac.th>
2021-11-26 17:31:26 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i=1;
+    int num[i];
+
+    while(num[i-1] != 0){
+        cout << "Enter an integer: ";
+        cin >> num[i];
+        i++;
+    }
+    int a=0,b=0,c=0;
+    while(a < i-1){
+        if(num[a]%2==0){
+            b++;
+        }else{
+            c++;
+        }
+        a++;
+    }
+  
+    cout << "#Even numbers = " << b << endl;
+    cout << "#Odd numbers = " << c-1;
     return 0;
 }
